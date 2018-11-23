77de5ce5c43dceab4bac415b589cb42972974d9f
ZakZheng <1258645603@qq.com>
2018-11-23 10:36:11 +0800

    ssr

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,6 +19,9 @@ module.exports = {
   // add your custom rules here
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'prettier/prettier': 'off',
+    'vue/max-attributes-per-line': 'off',
+    'vue/mustache-interpolation-spacing': 'off'
   }
 }
